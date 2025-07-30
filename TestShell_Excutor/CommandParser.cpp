4bef850df23be98c1c1b3675fc85afcf40c2a7e9
HyejinJung <aspiry@naver.com>
2025-07-30 09:40:54 +0900

    [add]LBA Check Function

--- TestShell_Excutor/CommandParser.cpp
+++ TestShell_Excutor/CommandParser.cpp
@@ -39,5 +39,33 @@ bool CommandParser::checkParamNum(vector<string> cmdSplits)
 	return false;
 }
 
+bool CommandParser::checkValidLBA(vector<string> cmdSplits)
+{
+	for (CommandFormat cmddata : commandlist)
+	{
+		if (cmddata.cmd == cmdSplits[0])
+		{
+			if (cmddata.isUseLBA)
+			{
+
+				string lbastr = cmdSplits[1];
+				if (lbastr.size() > 2)
+					return false;
+				
+				for (char lbach : lbastr)
+				{
+					if (lbach < '0' || lbach > '9')
+						return false;				
+				
+				}		
+				return true;
+			}
+			else
+				return true;
+		}
+	}
+	return false;
+}
+
 
 
