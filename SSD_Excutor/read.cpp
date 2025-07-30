ecaaa2018b67c8e90cfe71eab4553022050fccaf
hoonhur <hurhoon1012@gmail.com>
2025-07-30 15:08:13 +0900

    [refactor]ssd read

--- SSD_Excutor/read.cpp
+++ SSD_Excutor/read.cpp
@@ -8,21 +8,41 @@
 class Read {
 public:
 	void execute(unsigned int address) {
-		const std::string NAND_FILE = "ssd_nand.txt";
-		const std::string OUTPUT_FILE = "ssd_output.txt";
+		deletePrevOutputFile();
 
-		if (file_exists(OUTPUT_FILE)) {
-			if (std::remove(OUTPUT_FILE.c_str()) == 0) {
-			}
-			else {
-				std::cout << "fail to delete " << OUTPUT_FILE << std::endl;
-			}
+		std::string matchedValue = getValue(address);
+		writeOutputFile(matchedValue);
+		return;
+	}
+
+	void writeOutputFile(std::string& matchedValue)
+	{
+		std::ofstream out(OUTPUT_FILE);
+		if (out.is_open()) {
+			out << matchedValue;
+			out.close();
 		}
+		else {
+			std::cout << "fail to create output file";
+		}
+	}
 
-		std::string matchedValue = "0x00000000";
+private:
+	bool file_exists(const std::string& filename) {
+		std::ifstream file(filename);
+		return file.good();
+	}
+
+	int deleteFile(const std::string& fileName)
+	{
+		return std::remove(fileName.c_str());
+	}
+
+	std::string getValue(unsigned int address)
+	{
+		std::string ret = "0x00000000";
 		std::string line;
 		std::ifstream nand(NAND_FILE);
-
 		if (!nand.is_open()) {
 			std::cout << "fail to open : " << NAND_FILE << "\n";
 		}
@@ -37,26 +57,26 @@ public:
 				}
 
 				if (addr == address) {
-					matchedValue = value;
+					ret = value;
 				}
 			}
 			nand.close();
 		}
-
-		std::ofstream out(OUTPUT_FILE);
-		if (out.is_open()) {
-			out << matchedValue;
-			out.close();
-		}
-		else {
-			std::cout << "fail to create output file";
-		}
-
-		return;
+		return ret;
 	}
-private:
-	bool file_exists(const std::string& filename) {
-		std::ifstream file(filename);
-		return file.good();
+	
+	void deletePrevOutputFile()
+	{
+		if (file_exists(OUTPUT_FILE)) {
+			if (deleteFile(OUTPUT_FILE) == DELETE_SUCCESS) {
+			}
+			else {
+				std::cout << "fail to delete " << OUTPUT_FILE << std::endl;
+			}
+		}
 	}
+
+	const std::string NAND_FILE = "ssd_nand.txt";
+	const std::string OUTPUT_FILE = "ssd_output.txt";
+	const int DELETE_SUCCESS = 0;
 };
