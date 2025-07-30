0f47fa96a02080b74dc1344af3fb94e88764ea72
hoonhur <hurhoon1012@gmail.com>
2025-07-30 13:38:11 +0900

    translate korean to english

--- SSD_Excutor/read.cpp
+++ SSD_Excutor/read.cpp
@@ -11,12 +11,11 @@ public:
 		const std::string NAND_FILE = "ssd_nand.txt";
 		const std::string OUTPUT_FILE = "ssd_output.txt";
 
-		// 1. 기존 output 파일 존재 시 삭제
 		if (file_exists(OUTPUT_FILE)) {
 			if (std::remove(OUTPUT_FILE.c_str()) == 0) {
 			}
 			else {
-				std::cout << "파일 삭제 실패\n";
+				std::cout << "fail to delete " << OUTPUT_FILE << std::endl;
 			}
 		}
 
@@ -25,7 +24,7 @@ public:
 		std::ifstream nand(NAND_FILE);
 
 		if (!nand.is_open()) {
-			std::cout << "파일 열기 실패: " << NAND_FILE << "\n";
+			std::cout << "fail to open : " << NAND_FILE << "\n";
 		}
 		else {
 			while (std::getline(nand, line)) {
@@ -34,11 +33,11 @@ public:
 				std::istringstream iss(line);
 
 				if (!(iss >> addr >> value)) {
-					continue; // 형식이 잘못된 줄 무시
+					continue;
 				}
 
 				if (addr == address) {
-					matchedValue = value; // 최신 값으로 덮어쓰기
+					matchedValue = value;
 				}
 			}
 			nand.close();
@@ -50,7 +49,7 @@ public:
 			out.close();
 		}
 		else {
-			std::cout << "결과 파일 생성 실패\n";
+			std::cout << "fail to create output file";
 		}
 
 		return;
