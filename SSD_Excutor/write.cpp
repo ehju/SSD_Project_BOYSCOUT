16b82a5ff826995138fd7c2849a81806fd383628
uiyangco <jinsup13@naver.com>
2025-07-30 10:55:18 +0900

    [Refactor] SSD Write Refactorying SSD Write Refactorying

--- SSD_Excutor/write.cpp
+++ SSD_Excutor/write.cpp
@@ -9,7 +9,7 @@ class Write {
 public:
 	Write()
 	{
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < SSD_SIZE_PER_LBA; i++)
 		{
 			map[i] = 0;
 		}
@@ -19,9 +19,23 @@ public:
 
 		map[lba] = value;
 
+		flush();
+
+		return;
+	}
+
+private:
+	static const unsigned int START_LBA = 0;
+	static const unsigned int SSD_SIZE_PER_LBA = 100;
+
+	std::string nand = "ssd_nand.txt";
+	unsigned int map[SSD_SIZE_PER_LBA];
+
+	void flush()
+	{
 		std::ofstream file(nand, std::ios::out | std::ios::trunc);
-		
-		for (int i = 0; i < 100; i++)
+
+		for (int i = 0; i < SSD_SIZE_PER_LBA; i++)
 		{
 			std::string line = "";
 			std::ostringstream ss;
@@ -32,10 +46,5 @@ public:
 		}
 
 		file.close();
-
-		return;
 	}
-
-	std::string nand = "ssd_nand.txt";
-	unsigned int map[100];
 };
