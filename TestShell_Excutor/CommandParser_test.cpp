dd31e47053f62482447ae4cb4d1381e69a7e3669
Sewoon Park <seuni.park@samsung.com>
2025-07-30 14:24:35 +0900

    [add] new TC for RunCommandReadPass
    runCommandRead should print "[Read] LBA 3 : 0x00000000"
    
    Signed-off-by: Sewoon Park <seuni.park@samsung.com>

--- TestShell_Excutor/CommandParser_test.cpp
+++ TestShell_Excutor/CommandParser_test.cpp
@@ -40,15 +40,31 @@ TEST(CPTest, RunCommandCallRed) {
 	const string cmdline = "read 0";
 	EXPECT_EQ(CMD_BASIC_READ, cp.runCommand(cmdline));
 }
-TEST(CPTest, RunCommandWritePass) {
+TEST(CPTest, RunCommandWritePassDefault) {
 	CommandParser cp;
+	const string cmdline = "write 3 0xAAAABBBB";
 
 	std::stringstream buffer;
 	std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
-	int actual = cp.runCommandWrite("3", "0xAAAABBBB");
+	int expected = CMD_BASIC_WRITE;
+	int actual = cp.runCommand(cmdline);
 
 	std::cout.rdbuf(old);
 	string output = buffer.str();
-	EXPECT_EQ(true, actual);
+	EXPECT_EQ(expected, actual);
 	EXPECT_EQ(output, "[Write] Done\n");
+}
+TEST(CPTest, RunCommandReadPassDefault) {
+	CommandParser cp;
+	const string cmdline = "read 3";
+
+	std::stringstream buffer;
+	std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+	int expected = CMD_BASIC_READ;
+	int actual = cp.runCommand(cmdline);
+
+	std::cout.rdbuf(old);
+	string output = buffer.str();
+	EXPECT_EQ(expected, actual);
+	EXPECT_EQ(output, "[Read] LBA 3 : 0x00000000\n");
 }
