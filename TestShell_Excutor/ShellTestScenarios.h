825b71245bc9bc89c4a5a49a14bd4ffa890688a6
ehju <eh13.ju@samsung.com>
2025-07-31 12:43:35 +0900

    [refactor] devide code file by classes

--- TestShell_Excutor/ShellTestScenarios.h
+++ TestShell_Excutor/ShellTestScenarios.h
@@ -1,1 +1,16 @@
 #pragma once
+class TestScenario {
+	virtual bool run();
+};
+class FullWriteAndReadCompare : public TestScenario
+{
+	bool run() override;
+};
+class PartialLBAWrite : public TestScenario
+{
+	bool run() override;
+};
+class WriteReadAging : public TestScenario
+{
+	bool run() override;
+};
