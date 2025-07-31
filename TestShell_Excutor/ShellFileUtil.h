120ce3d60c75a7236c659ba221cd3d1f20b28106
sh1lee <hongikdiver@hotmail.com>
2025-08-01 08:56:48 +0900

    [add] ShellFileUtil for logger

---
+++ TestShell_Excutor/ShellFileUtil.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>	
+class ShellFileUtil {
+public:
+    static void deletePrevOutputFile();
+    static void writeOutputFile(std::string& writeString);
+    static void renameLogFile();
+
+private:
+    static bool isFileTooLarge(const std::string& filename, std::size_t maxSize);
+    static bool file_exists(const std::string& filename);
+    static int deleteFile(const std::string& fileName);
+    static std::string makeTimestampedFilename();
+    inline static const int DELETE_SUCCESS = 0;
+    inline static const std::string OUTPUT_FILE = "latest.log";
+};
