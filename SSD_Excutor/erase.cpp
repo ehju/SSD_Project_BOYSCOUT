81fdde78a6c788eff0dd06341e64923f507a11ec
hoonhur <hurhoon1012@gmail.com>
2025-08-01 18:45:05 +0900

    refactor

--- SSD_Excutor/erase.cpp
+++ SSD_Excutor/erase.cpp
@@ -1,8 +1,14 @@
 #include "erase.h"
 
 void Erase::execute(CommandInfo commandInfo) {
-	unsigned int eraseValue = static_cast<unsigned int>(0x0);
-	for (unsigned int i = 0;i < commandInfo.value;i++) {
-		write.execute(CommandInfo{ static_cast<unsigned int>(SSDCommand::SSDCommand_WRITE), commandInfo.lba + i, eraseValue });
+	unsigned int eraseRange = commandInfo.value;
+	for (unsigned int i = 0;i < eraseRange;i++) {
+		doErase(commandInfo.lba+ i);
 	}
+}
+
+void Erase::doErase(unsigned int address)
+{
+	static unsigned int eraseValue = static_cast<unsigned int>(0x0);
+	write.execute(CommandInfo{ static_cast<unsigned int>(SSDCommand::SSDCommand_WRITE), address, eraseValue });
 }
