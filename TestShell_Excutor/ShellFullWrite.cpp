607e65b17f42a81695e4ae5fae03249da8d53f02
ehju <eh13.ju@samsung.com>
2025-08-01 14:00:12 +0900

    [feature] print Writedone

--- TestShell_Excutor/ShellFullWrite.cpp
+++ TestShell_Excutor/ShellFullWrite.cpp
@@ -4,11 +4,17 @@ bool FullWrite::execute(CommandInfo cmdInfo)
 	Logger::getInstance()->print(__FUNCTION__, "called");
 	unsigned int lba = cmdInfo.lba;
 	unsigned int data = cmdInfo.value;
+	bool ret =false;
 	for (int lba = LBA_MIN; lba <= LBA_MAX; lba++) {
+
 		if (!ssd->write(lba, data)) {
 			Logger::getInstance()->print(__FUNCTION__, "error on write %d %d", lba, data);
 			return false;
 		}
 	}
+	printWriteDone();
 	return true;
+}
+void FullWrite::printWriteDone() {
+	std::cout << "[Write] Done" << "\n";
 }
--- TestShell_Excutor/ShellFullWrite.h
+++ TestShell_Excutor/ShellFullWrite.h
@@ -9,6 +9,7 @@ public:
 	bool execute(CommandInfo cmdInfo) override;
 private:
 	iTS_SSD* ssd;
+	void printWriteDone();
 	const int LBA_MAX = 99;
 	const int LBA_MIN = 0;
 };
