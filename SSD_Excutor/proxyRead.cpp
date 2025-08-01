29280d53fa6edbafc7a69a24843a9c3ff4c32ae0
hoonhur <hurhoon1012@gmail.com>
2025-08-01 13:23:49 +0900

    refactor proxyRead

--- SSD_Excutor/proxyRead.cpp
+++ SSD_Excutor/proxyRead.cpp
@@ -1,63 +1,73 @@
 #include "proxyRead.h"
 
 ProxyRead::ProxyRead() {
-    cbm = &(CommandBufferManager::getInstance());
+	cbm = &(CommandBufferManager::getInstance());
 }
 
 ProxyRead::ProxyRead(CommandBufferManager* ptr) : cbm{ ptr } {}
 
 void ProxyRead::execute(CommandInfo commandInfo) {
-    if (true == bufferHit(commandInfo.lba)) {
-        FileUtil::deletePrevOutputFile();
-        std::string matchedValue = getHexValueFromBuffer(commandInfo.lba);
-        FileUtil::writeOutputFile(matchedValue);
-    }
-    else realRead.execute(commandInfo);
-    return;
+	if (true == bufferHit(commandInfo.lba)) {
+		FileUtil::deletePrevOutputFile();
+		std::string matchedValue = getHexValueFromBuffer(commandInfo.lba);
+		FileUtil::writeOutputFile(matchedValue);
+	}
+	else realRead.execute(commandInfo);
+	return;
 }
 std::string ProxyRead::getHexValueFromBuffer(unsigned int address)
 {
-    std::vector<DetailedCommandInfo>commandBufferVector = cbm->getCommandBufferList();
+	std::vector<DetailedCommandInfo>commandBufferVector = cbm->getCommandBufferList();
 
-    for (int i = commandBufferVector.size() - 1; i >= 0; --i) {
-        if (commandBufferVector[i].commandInfo.command == static_cast<int>(SSDCommand::SSDCommand_WRITE)) {
-            if (commandBufferVector[i].commandInfo.lba == address) {
-                return toHexString(commandBufferVector[i].commandInfo.value);
-            }
-        }
-        else if (commandBufferVector[i].commandInfo.command == static_cast<int>(SSDCommand::SSDCommand_ERASE)) {
-            if (commandBufferVector[i].commandInfo.lba <= address && commandBufferVector[i].commandInfo.lba + commandBufferVector[i].commandInfo.value > address) {
-                return "0x00000000";
-            }
-        }
-    }
-    return "";
+	int vectorSize = static_cast<int>(commandBufferVector.size());
+	for (int i = vectorSize - 1; i >= 0; --i) {
+		if (isBufferHitWriteCommand(commandBufferVector[i], address)) {
+			return toHexString(commandBufferVector[i].commandInfo.value);
+		}
+		else if (isBufferHitEraseCommand(commandBufferVector[i], address)) {
+			return "0x00000000";
+		}
+	}
+	return "";
 }
+
+bool ProxyRead::isBufferHitWriteCommand(const DetailedCommandInfo& detailedCommandInfo, int address) {
+	if (detailedCommandInfo.commandInfo.command == static_cast<int>(SSDCommand::SSDCommand_WRITE)) {
+		if (detailedCommandInfo.commandInfo.lba == address) return true;
+	}
+	return false;
+}
+
+bool ProxyRead::isBufferHitEraseCommand(const DetailedCommandInfo& detailedCommandInfo, int address) {
+	if (detailedCommandInfo.commandInfo.command == static_cast<int>(SSDCommand::SSDCommand_ERASE)) {
+		if (detailedCommandInfo.commandInfo.lba <= address && detailedCommandInfo.commandInfo.lba + detailedCommandInfo.commandInfo.value > address)
+			return true;
+	}
+	return false;
+}
+
 bool ProxyRead::bufferHit(unsigned int address) {
-    std::vector<DetailedCommandInfo>commandBufferVector = cbm->getCommandBufferList();
-   // 주소를 받으면 address에 해당하는 command가 있는지 역순으로 탐색
-    for (int i = commandBufferVector.size() - 1; i >= 0; --i) {
-        if (commandBufferVector[i].commandInfo.command == static_cast<int>(SSDCommand::SSDCommand_WRITE)) {
-            if (commandBufferVector[i].commandInfo.lba == address) {
-                return true;
-            }
-        }
-        else if (commandBufferVector[i].commandInfo.command == static_cast<int>(SSDCommand::SSDCommand_ERASE)) {
-            if (commandBufferVector[i].commandInfo.lba <= address && commandBufferVector[i].commandInfo.lba + commandBufferVector[i].commandInfo.value > address) {
-                return true;
-            }
-        }
-    }
-    return false;
+	std::vector<DetailedCommandInfo>commandBufferVector = cbm->getCommandBufferList();
+
+	int vectorSize = static_cast<int>(commandBufferVector.size());
+	for (int i = vectorSize - 1; i >= 0; --i) {
+		if (isBufferHitWriteCommand(commandBufferVector[i], address)) {
+				return true;
+			}
+		else if (isBufferHitEraseCommand(commandBufferVector[i], address)) {
+				return true;
+			}
+	}
+	return false;
 }
 
 std::string ProxyRead::toHexString(unsigned int value) {
-    std::stringstream ss;
-    ss << "0x"
-        << std::uppercase            // 대문자
-        << std::setfill('0')        // 빈 자리는 0으로 채움
-        << std::setw(8)             // 항상 8자리로 맞춤
-        << std::hex                 // 16진수로 출력
-        << value;
-    return ss.str();
+	std::stringstream ss;
+	ss << "0x"
+		<< std::uppercase
+		<< std::setfill('0')
+		<< std::setw(8)
+		<< std::hex
+		<< value;
+	return ss.str();
 }
--- SSD_Excutor/proxyRead_test.cpp
+++ SSD_Excutor/proxyRead_test.cpp
@@ -1,6 +1,7 @@
 #include "gmock/gmock.h"
 #include "read.h"
 #include "write.h"
+#include "erase.h"
 #include "SSDHelper.h"
 #include "command_buffer_manager.h"
 #include "proxyRead.h"
@@ -26,39 +27,75 @@ TEST_F(ProxyReadTestFixture, HitBuffer)
 {
     //buffer에서 가져온 값이 hit 되어야 한다.
     std::vector<DetailedCommandInfo> stubList;
-    unsigned int cmd = 0;
-    unsigned int param1 = 0x1;
-    unsigned int param2 = 0x2;
+    unsigned int cmd = static_cast<unsigned int>(0);
+    unsigned int targetAddress = static_cast<unsigned int>(0x3);
+    unsigned int value = static_cast<unsigned int>(0x3);
     std::shared_ptr<ICommand> stubCmd = std::make_shared<Write>();
-    stubList.push_back(DetailedCommandInfo{ CommandInfo{cmd, param1,param2}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{cmd, targetAddress,value}, stubCmd });
 
     EXPECT_CALL(mock, getCommandBufferList)
         .WillOnce(Return(stubList));
-    EXPECT_EQ(proxyRead.bufferHit(param1), true);
+    EXPECT_EQ(proxyRead.bufferHit(targetAddress), true);
+}
+
+TEST_F(ProxyReadTestFixture, HitBufferWriteCommand)
+{
+    //buffer에서 가져온 값이 hit 되어야 한다.
+    std::vector<DetailedCommandInfo> stubList;
+    std::shared_ptr<ICommand> stubCmd = std::make_shared<Write>();
+    unsigned int targetAddress = static_cast<unsigned int>(0x3);
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{0, 1, 1}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{0, 2, 2}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{0, targetAddress, 3}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{0, 4, 4}, stubCmd });
+
+    EXPECT_CALL(mock, getCommandBufferList)
+        .WillRepeatedly(Return(stubList));
+    EXPECT_EQ(proxyRead.bufferHit(targetAddress), true);
+}
+
+TEST_F(ProxyReadTestFixture, HitBufferEraseCommand)
+{
+    //buffer에서 가져온 값이 hit 되어야 한다.
+    std::vector<DetailedCommandInfo> stubList;
+    std::shared_ptr<ICommand> stubCmd = std::make_shared<Write>();
+    unsigned int eraseStartAddress = static_cast<unsigned int>(0x11);
+    unsigned int eraseRange = static_cast<unsigned int>(0x5);
+    
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{0, 1, 1}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{0, 2, 2}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{2, eraseStartAddress, eraseRange}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{0, 4, 4}, stubCmd });
+
+    EXPECT_CALL(mock, getCommandBufferList)
+        .WillRepeatedly(Return(stubList));
+    unsigned int targetAddress = static_cast<unsigned int>(0x15);
+    EXPECT_EQ(proxyRead.bufferHit(targetAddress), true);
 }
 
 // Fail to hit buffer
 TEST_F(ProxyReadTestFixture, FailToHitBuffer)
 {
     std::vector<DetailedCommandInfo> stubList;
-    unsigned int cmd = 0;
-    unsigned int param1 = 0x1;
-    unsigned int param2 = 0x2;
+    unsigned int cmd = static_cast<unsigned int>(0);
+    unsigned int nonTargAddress = 0x1;
+    unsigned int targetAddress = static_cast<unsigned int>(0x3);
+    unsigned int value = static_cast<unsigned int>(0x2);
     std::shared_ptr<ICommand> stubCmd = std::make_shared<Write>();
-    stubList.push_back(DetailedCommandInfo{ CommandInfo{cmd, param1,param2}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{cmd, nonTargAddress,value}, stubCmd });
     EXPECT_CALL(mock, getCommandBufferList)
-        .WillOnce(Return(stubList));
-    EXPECT_EQ(proxyRead.bufferHit((unsigned int)0x2), false);
+        .WillRepeatedly(Return(stubList));
+    EXPECT_EQ(proxyRead.bufferHit(targetAddress), false);
 }
 
 // Read From buffer
-TEST_F(ProxyReadTestFixture, ReadFromBuffer)
+TEST_F(ProxyReadTestFixture, ReadFromBufferWithWriteCommand)
 {
     std::vector<DetailedCommandInfo> stubList;
-    unsigned int cmd = 0;
-    unsigned int param1 = 0x31;
-    unsigned int param2 = 0x32;
-    std::shared_ptr<ICommand> stubCmd = std::make_shared<Write>();
+    unsigned int cmd = static_cast<unsigned int>(0);
+    unsigned int param1 = static_cast<unsigned int>(0x31);
+    unsigned int param2 = static_cast<unsigned int>(0x32);
+    std::shared_ptr<ICommand> stubCmd = std::make_shared<Erase>();
     stubList.push_back(DetailedCommandInfo{ CommandInfo{ cmd, param1,param2}, stubCmd });
 
     EXPECT_CALL(mock, getCommandBufferList)
@@ -66,5 +103,40 @@ TEST_F(ProxyReadTestFixture, ReadFromBuffer)
 
     EXPECT_EQ(proxyRead.getHexValueFromBuffer(param1), "0x00000032");
     EXPECT_THAT(proxyRead.getHexValueFromBuffer(param1), Ne(""));
+}
+
+TEST_F(ProxyReadTestFixture, ReadFromBufferWithEraseCommand)
+{
+    std::vector<DetailedCommandInfo> stubList;
+    unsigned int cmd = 2;
+    unsigned int targetAddress = 0x31;
+    unsigned int targetRange = 0x10;
+    std::shared_ptr<ICommand> stubCmd = std::make_shared<Erase>();
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{ cmd, targetAddress,targetRange}, stubCmd });
+
+    EXPECT_CALL(mock, getCommandBufferList)
+        .WillRepeatedly(Return(stubList));
+
+    unsigned int edgeAddress = targetAddress + targetRange - 1;
+    EXPECT_EQ(proxyRead.getHexValueFromBuffer(edgeAddress), "0x00000000");
+    EXPECT_THAT(proxyRead.getHexValueFromBuffer(edgeAddress), Ne(""));
+}
+
+TEST_F(ProxyReadTestFixture, ReadFromBufferWithComplexCommand)
+{
+    std::vector<DetailedCommandInfo> stubList;
+    std::shared_ptr<ICommand> stubCmd = std::make_shared<Write>();
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{ 0, 1, 1}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{ 0, 2, 2}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{ 0, 3, 3}, stubCmd });
+    stubList.push_back(DetailedCommandInfo{ CommandInfo{ 2, 2, 2}, stubCmd });
+
+    EXPECT_CALL(mock, getCommandBufferList)
+        .WillRepeatedly(Return(stubList));
+
+    EXPECT_EQ(proxyRead.getHexValueFromBuffer(2), "0x00000000");
+    EXPECT_EQ(proxyRead.getHexValueFromBuffer(3), "0x00000000");
+    EXPECT_THAT(proxyRead.getHexValueFromBuffer(1), Ne("0x00000000"));
+    
 }
 #endif
